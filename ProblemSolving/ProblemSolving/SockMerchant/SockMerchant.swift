057e394170f38e21616ec6a81aec7afefaaf96a5
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-05-23 15:23:49 +0530

    Added first problem solution from (Hacker Rank - Sock Merchant)

---
+++ ProblemSolving/ProblemSolving/SockMerchant/SockMerchant.swift
@@ -0,0 +1,35 @@
+//
+//  SockMerchant.swift
+//  ProblemSolving
+//
+//  Created by panditpakhurde on 23/05/20.
+//  Copyright © 2020 macpandit. All rights reserved.
+//
+
+import Foundation
+
+///Finding Marching Pairs of socks.
+///
+///Used 'Set<Int>' i.e. unordered collection of unique elements
+///which ensure that each element appears only once in a collection
+
+func sortMerchant(_ n: Int, ar: [Int]) {
+    var pairs: Int = 0
+    
+    var set: Set<Int> = []
+    
+    for e in ar {
+        if set.contains(e) {
+            set.remove(e)
+            pairs += 1
+        }else {
+            set.insert(e)
+        }
+    }
+    print("Problem 1: Sock Merchant")
+    print("maching pairs: \(pairs)")
+    print("---------------------------------")
+
+
+}
+
---
+++ ProblemSolving/ProblemSolving/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  ProblemSolving
+//
+//  Created by panditpakhurde on 23/05/20.
+//  Copyright © 2020 macpandit. All rights reserved.
+//
+
+import Foundation
+
+//Problem - Sock Merchant - Hacker Rank
+//Finding Marching Pairs of socks.
+sortMerchant(9, ar: [1, 4, 2, 7, 5, 4, 2, 4, 1, 5])
+
